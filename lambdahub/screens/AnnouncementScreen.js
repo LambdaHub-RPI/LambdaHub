f271e64cdb6216b08b3fcec01cbb3cd22cf6e775
Adriano Andrade <andraa2@rpi.edu>
2024-01-30 17:43:21 -0500

    Created buttons to add and remove announcements

--- lambdahub/screens/AnnouncementScreen.js
+++ lambdahub/screens/AnnouncementScreen.js
@@ -1,12 +1,48 @@
+import React from 'react';
 import { View, Text, StyleSheet } from "react-native";
+import { TextInput } from 'react-native-gesture-handler';
+import { Button } from 'react-native';
 
 export default function AnnouncementScreen() {
-    return (
-      <View style={styles.container}>
-        <Text>Announcement Screen</Text>
+  const [input, setInput] = React.useState([''])
+  
+  const handleAdd = () => {
+    const newInputs = [...input];
+    newInputs.push('');
+    setInput(newInputs);
+  };
+
+  const handleRemove = (index) => {
+    const newInputs = [...input];
+    newInputs.splice(index, 1);
+    setInput(newInputs);
+  };
+
+  const handleInputChange = (text, index) => {
+    const newInputs = [...input];
+    newInputs[index] = text;
+    setInput(newInputs);
+  };
+  
+  
+  return (
+    <View style={styles.container}>
+      <View>
+        {input.map((input, index) => (
+          <View key={index} style={styles.inputContainer}>
+            <TextInput
+              value={input}
+              onChangeText={text => handleInputChange(text, index)}
+              style={styles.input}
+            />
+            <Button title="remove" onPress={() => handleRemove(index)} />
+          </View>
+        ))}
+        <Button title="add" onPress={handleAdd} />
       </View>
-    );
-  }
+    </View>
+  );
+};
   
   const styles = StyleSheet.create({
     container: {
@@ -14,5 +50,25 @@ export default function AnnouncementScreen() {
       backgroundColor: '#fff',
       alignItems: 'center',
       justifyContent: 'center',
+    },
+
+    inputContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      borderWidth: 1,
+      borderColor: '#ccc',
+      marginBottom: 10,
+      padding: 5,
+      width: 300,
+      height: 50
+    },
+    
+    input: {
+      width: 200,
+      height: 40,
+      borderColor: 'gray',
+      borderWidth: 1,
+      marginRight: 10,
+      paddingLeft: 10,
     },
   });
