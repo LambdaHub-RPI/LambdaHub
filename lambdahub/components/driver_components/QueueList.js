8f5b71f98a9d6cb56e1929c09dcc3b74e7486608
unknown <greenberg.jason2004@gmail.com>
2024-04-24 20:05:52 -0400

    added pull to refresh, infinite scroll, and error handling to my queuelist

--- lambdahub/components/driver_components/QueueList.js
+++ lambdahub/components/driver_components/QueueList.js
@@ -1,16 +1,41 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
+import React, { useState, useCallback } from 'react';
+import { View, FlatList, RefreshControl } from 'react-native';
 import QueueItem from './QueueItem'; // Ensure the import path is correct based on your project structure
 
-const QueueList = ({ data, onDelete }) => {
-  // this is going to pass onDelete to each ride, then when the ride is swiped it is going to call the deleteRide
-  // in queuescreen so that when you swipe on a ride it is going to call that function and make the delete request.
+const QueueList = ({ data, onDelete, onLoadMore, onRefresh }) => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  // Function to handle the refresh action
+  const handleRefresh = useCallback(() => {
+    setRefreshing(true);
+    onRefresh()
+      .then(() => setRefreshing(false))
+      .catch(() => setRefreshing(false)); // Make sure to handle errors as well
+  }, [onRefresh]);
+
+  // Function to render each item in the list
+  const renderItem = ({ item }) => (
+    <QueueItem data={item} onDelete={onDelete} />
+  );
+
+  // Function to provide a unique key for each item
+  const keyExtractor = (item) => item.identifier.toString();
+
   return (
     <View style={{ flex: 1, width: '100%' }}>
       <FlatList
         data={data}
-        keyExtractor={(item) => item.identifier.toString()}
-        renderItem={({ item }) => <QueueItem data={item} onDelete={onDelete} />}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        refreshControl={
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
+        }
+        onEndReached={onLoadMore}
+        onEndReachedThreshold={0.5} // Customize the threshold depending on the behavior you want
+        initialNumToRender={10} // Adjust number based on performance and data size
       />
     </View>
   );
